b80bfe0326bdb0584c27dad0022c0ec511ae3bda
jerome.p.lefebvre@gmail.com <jerome.p.lefebvre@gmail.com>
2014-05-15 10:39:00 -0700

    Fix bytes to string

--- hello.py
+++ hello.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-	name = redis.get('name')
+	name = redis.get('name').decode('utf-8')
 	return "<big><b>hello %s</b></big>" % name
 
 if __name__ == "__main__":
