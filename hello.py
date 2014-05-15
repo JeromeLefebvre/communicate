3b772e17f4fcde2217a3c24148a573a5422dd350
jerome.p.lefebvre@gmail.com <jerome.p.lefebvre@gmail.com>
2014-05-15 10:56:29 -0700

    Added a template

--- hello.py
+++ hello.py
@@ -1,13 +1,24 @@
 
-from flask import Flask
+from flask import Flask, render_template
 from store import redis
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello():
-	name = redis.get('name').decode('utf-8')
-	return "<big><b>hello %s</b></big>" % name
+    #name = redis.get('name').decode('utf-8')
+    name = "Chris"
+    return render_template("communicate.html", name=name)
+
+
+@app.route('/', methods=['POST'])
+def my_form_post():
+    print("Called")
+    if request.form['my-form'] == 'Send':
+        return render_template("communicate.html", name='send')
+    elif request.form['my-form'] == 'Refresh':
+        return render_template("communicate.html", name='refresh')
 
 if __name__ == "__main__":
 	app.run()
