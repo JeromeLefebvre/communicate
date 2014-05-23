3341d67b495e7c0861ec888b5b7ee9aab8b2e756
jerome.p.lefebvre@gmail.com <jerome.p.lefebvre@gmail.com>
2014-05-22 19:00:50 -0700

    Created 3 named tuple for testing

--- lookup.py
+++ lookup.py
@@ -1,8 +1,32 @@
 import amazonproduct
 from lxml import etree
 
+from collections import namedtuple
+
+Book = namedtuple("book", ["title", "price", "isbn"], verbose=False)
+
+isbns = [line.rstrip() for line in open("isbn_db.txt")]
+
 api = amazonproduct.API(locale='us')
-items = api.call(Operation='ItemLookup',SearchIndex='Books',IdType='ISBN',ItemId='9780521375108', ResponseGroup='ItemAttributes')
+def amazon_lookup(isbn):
+	#items = api.call(Operation='ItemLookup',SearchIndex='Books',IdType='ISBN',ItemId='9780521375108', ResponseGroup='ItemAttributes')
+	print(isbn)
+	items = api.call(Operation='ItemLookup',SearchIndex='Books',IdType='ISBN',ItemId=isbn, ResponseGroup='ItemAttributes')
+	if len(items) > 1:
+		print(isbn)
+		raise ValueError
+	item = items[0]
+	name = item.find('.//aws:Title', namespaces={'aws': "http://webservices.amazon.com/AWSECommerceService/2011-08-01"}).text
+	price = item.find('.//aws:FormattedPrice', namespaces={'aws': "http://webservices.amazon.com/AWSECommerceService/2011-08-01"}).text
+	book = Book(name, price, isbn)
+
+books = []
+for isbn in isbns:
+	books.append(amazon_lookup(isbn))
+
+
+
+'''
 #items = api.item_lookup('9780521375108', IdType='ISBN', SearchIndex='Books', )
 #items = api.item_lookup('052137510X', IdType='ISBN', SearchIndex='Books')
 for item in items:
@@ -25,3 +49,4 @@ print(item.find('.//aws:Author', namespaces={'aws': "http://webservices.amazon.c
 #q = etree.fromstring('<xml><hello>a</hello><x><hello>b</hello></x></xml>')
 
 #print(q.findall('.//hello')[0].text)
+'''
