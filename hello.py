734c813f8a6c21f6dab5922dcda4bd06fab27f88
jerome.p.lefebvre@gmail.com <jerome.p.lefebvre@gmail.com>
2014-05-22 21:08:06 -0700

    Fixing hello to use GET

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, render_template
 from redis_to_tuple import allBooks
+from flask.ext.mail import Mail, Message
 
 app = Flask(__name__)
 
@@ -17,20 +18,19 @@ app.config.update(dict(
 
 mail = Mail(app)
 
-@app.route('/', methods=['GET','POST'])
+@app.route('/', methods=['GET'])
 def communicate_post():
     a = request.args.getlist('wanted') 
     if len(a) == 0:
         entries = books
         return render_template("comm.html", entries=entries)
     else:
-        #if request.form['my-form'] == 'Send':
-        email = request.form['text']
-        msg =  Message("Hello", sender="jeorme.8675309@gmail.com",  recipients=["jeorme.8675309@gmail.com"])
-        msg.body = 'I am a roboot tester.'
+        email = request.args['text']
+        msg =  Message("Hello here are the requested books", sender="jeorme.8675309@gmail.com",  recipients=["jeorme.8675309@gmail.com"])
+        msg.body = str(a)
         msg.body += email
         mail.send(msg)
-        return 'Thank you!'
+    return 'Thank you!'
 
 if __name__ == "__main__":
     app.run(debug=True)
