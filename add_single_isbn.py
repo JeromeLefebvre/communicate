058a89f73b58d4c7f3926a959657b1080b7dce45
jerome.p.lefebvre@gmail.com <jerome.p.lefebvre@gmail.com>
2014-05-25 13:45:15 -0700

    Refactored the adding of a single isbn

--- add_single_isbn.py
+++ add_single_isbn.py
@@ -1,15 +1,8 @@
 
 import sys
 
-print(sys.argv[1])
-
-from Book import Book
-from lookup import amazon_lookup
-from store import redis
+from isbn_to_redis import store_to_redis
 
 isbns = [line.rstrip() for line in sys.argv[1:]]
 
-for isbn in isbns:
-	book = amazon_lookup(isbn)
-	redis.sadd("books", book.to_string())
-
+store_to_redis(isbns)
--- isbn_to_redis.py
+++ isbn_to_redis.py
@@ -3,12 +3,15 @@ from lookup import amazon_lookup
 from store import redis
 from isbn_to_image import isbn_to_saved_image
 
-isbns = [line.rstrip() for line in open("isbn_db.txt")]
+def store_to_redis(isbns):
+	for isbn in isbns:
+		book = amazon_lookup(isbn)
+		# Adding to the list of books
+		redis.sadd("books", book.to_string())
+		# so that the book data can be access later using the isbn
+		redis.set(isbn, book.to_string())
+		isbn_to_saved_image(isbn)
 
-for isbn in isbns:
-	book = amazon_lookup(isbn)
-	# Adding to the list of books
-	redis.sadd("books", book.to_string())
-	# so that the book data can be access later using the isbn
-	redis.set(isbn, book.to_string())
-	isbn_to_saved_image(isbn)
+if __name__ == "__main__":
+	isbns = [line.rstrip() for line in open("isbn_db.txt")]
+	store_to_redis(isbns)
