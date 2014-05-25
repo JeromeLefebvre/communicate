35004b0c21bf3e4825ebede28b85ee4d026a1251
jerome.p.lefebvre@gmail.com <jerome.p.lefebvre@gmail.com>
2014-05-25 09:19:06 -0700

    Cleaned up the isbn_to_image file

--- isbn_to_image.py
+++ isbn_to_image.py
@@ -1,5 +1,6 @@
 import requests
 
+# Based on the info from https://openlibrary.org/dev/docs/api/covers
 def isbn_to_saved_image(isbn):
     def cover_urls(isbn):
         return ["http://covers.openlibrary.org/b/isbn/" + isbn + '-' + size + ".jpg" for size in ['L', 'M', 'S']][0]
@@ -12,27 +13,3 @@ def isbn_to_saved_image(isbn):
                 f.write(chunk)
     else:
         raise ValueError("Didn't get the ok to save the image " + isbn)
-
-'''
-Initial try using a google database, did not work out in the end
-import urllib
-import json
-
-json_url = 'https://www.googleapis.com/books/v1/volumes?q=isbn:' + isbn
-
-json_document = urllib.urlopen(json_url)
-
-json_text = ''.join(json_document.readlines())
-
-j = json.loads(json_text)
-
-print(j["items"][0]["volumeInfo"])
-print([l["imageLinks"] for l in j["items"][0]["volumeInfo"].values()])
-
-#extracts = [p["extract"] for p in json["query"]["pages"].values()]
-
-thumbnail_url = j["items"][0]["volumeInfo"]["imageLinks"]["thumbnail"]
-
-print(thumbnail_url)
-#urllib.urlretrieve(thumbnail_url, "cover/" + isbn + ".png")
-'''
