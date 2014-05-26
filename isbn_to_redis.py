bd058db952006acf591998bcdd07354d095392a4
jerome.p.lefebvre@gmail.com <jerome.p.lefebvre@gmail.com>
2014-05-26 16:48:04 -0700

    adding a new isbn to isbn db, fixes #13

--- isbn_to_redis.py
+++ isbn_to_redis.py
@@ -3,15 +3,18 @@ from lookup import amazon_lookup
 from store import redis
 from isbn_to_image import isbn_to_saved_image
 
+
 def store_to_redis(isbns):
-	for isbn in isbns:
-		book = amazon_lookup(isbn)
-		# Adding to the list of books
-		redis.sadd("books", book.to_string())
-		# so that the book data can be access later using the isbn
-		redis.set(isbn, book.to_string())
-		isbn_to_saved_image(isbn)
+    for isbn in isbns:
+        book = amazon_lookup(isbn)
+        # Adding to the list of books
+        redis.sadd("books", book.to_string())
+        # so that the book data can be access later using the isbn
+        redis.set(isbn, book.to_string())
+        isbn_to_saved_image(isbn)
+        with open("isbn_db.txt", "a") as myfile:
+            myfile.write(isbn + "\n")
 
 if __name__ == "__main__":
-	isbns = [line.rstrip() for line in open("isbn_db.txt")]
-	store_to_redis(isbns)
+    isbns = [line.rstrip() for line in open("isbn_db.txt")]
+    store_to_redis(isbns)
